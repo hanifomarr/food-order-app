9bbeb10c06f69e50e767bfce95a27792dd8efdcb
Hanif Omar <hanif.omarr@gmail.com>
2023-09-29 11:19:02 +0800

    feat: meal summary components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import MealList from "./MealList";
+
+function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <MealList />
+    </>
+  );
+}
+
+export default Meals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+function MealsSummary() {
+  return (
+    <div className={classes.summary}>
+      <h2>Summary</h2>
+      <p>loremadasddasds sadad</p>
+      <p>sdasdsdsadsadasdsa</p>
+    </div>
+  );
+}
+
+export default MealsSummary;
