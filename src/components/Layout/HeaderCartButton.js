112c43b15714eb65c4d6f525306a43b7502f89b2
Hanif Omar <hanif.omarr@gmail.com>
2023-10-04 17:27:17 +0800

    feat: add bump animation when item.length changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/CartContext";
 
 function HeaderCartButton({ onClick }) {
-  const cartCtx = useContext(CartContext);
+  const [btnIsFocus, setBtnIsFocus] = useState(false);
+  const { item } = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.item.reduce((curNumber, item) => {
+  const numberOfCartItems = item.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${btnIsFocus ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (item.length === 0) {
+      return;
+    }
+    setBtnIsFocus(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsFocus(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [item]);
+
   return (
-    <button onClick={onClick} className={classes.button}>
+    <button onClick={onClick} className={buttonClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
