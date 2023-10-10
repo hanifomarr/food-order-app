1bf60c3a62d8cbc8f53b70ef6c7a66a5f3e9158b
Hanif Omar <hanif.omarr@gmail.com>
2023-10-10 15:13:10 +0800

    feat: error handling

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -5,29 +5,56 @@ import MealItem from "./MealItem/MealItem";
 
 function MealList() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState();
+
   const fetchMeals = useCallback(async () => {
-    const res = await fetch(
-      "https://react-http-1808d-default-rtdb.firebaseio.com/meals.json"
-    );
-    const data = await res.json();
+    try {
+      const res = await fetch(
+        "https://react-http-1808d-default-rtdb.firebaseio.com/meals"
+      );
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await res.json();
 
-    const loadedMeals = [];
+      const loadedMeals = [];
 
-    for (const key in data) {
-      loadedMeals.push({
-        id: key,
-        name: data[key].name,
-        desc: data[key].description,
-        price: data[key].price,
-      });
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          desc: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(error.message);
     }
-
-    setMeals(loadedMeals);
   }, []);
 
   useEffect(() => {
     fetchMeals();
   }, [fetchMeals]);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>...Loading</p>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{isError}</p>
+      </section>
+    );
+  }
 
   return (
     <div className={classes.meals}>
